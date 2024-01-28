9126c5d5709d232212f8a4c2ca46a06bab1059f4
Jay Mishra <148852132+akascientist@users.noreply.github.com>
2024-01-28 18:30:00 +0530

    Frontend mein bootstrap 4 install kr diya gya hai or uske sath hi bootstrap vue 2.x bhi

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 
-createApp(App).mount("#app");
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Create a new Vue application
+const app = createApp(App);
+
+// Make BootstrapVue available throughout your project
+app.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+app.use(IconsPlugin);
+
+// Mount the application
+app.mount("#app");

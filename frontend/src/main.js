2a5ce7ce8d070bf7e430e778334636d9081b8a54
Jay Mishra <148852132+akascientist@users.noreply.github.com>
2024-01-28 18:52:27 +0530

    Changes frontend from vue 3 to vue 2 as bootstrap 4 is not supporting on vue 3

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,19 +1,8 @@
-import { createApp } from "vue";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
+import Vue from 'vue'
+import App from './App.vue'
 
-// Import Bootstrap and BootstrapVue CSS files (order is important)
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+Vue.config.productionTip = false
 
-// Create a new Vue application
-const app = createApp(App);
-
-// Make BootstrapVue available throughout your project
-app.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
-app.use(IconsPlugin);
-
-// Mount the application
-app.mount("#app");
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
